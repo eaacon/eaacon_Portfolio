d4463ecf92cfd5f801e620571aa5a09be98f7e0a
Andrew Chen <andrewgychen@gmail.com>
2023-11-12 05:44:48 -0600

    Project Styling And Pages
    Created base for projects, cleaned up and split images from text, still need to couple the timelines of both. Missing some videos and gifs of games.

--- Javascripts/nav.js
+++ Javascripts/nav.js
@@ -42,3 +42,20 @@ function CloseModal()
 {
     document.getElementById("modal").style.display = "none";
 }
+
+var ToCClick = document.querySelector('#ToC')
+if(ToCClick != null)
+{
+    ToCClick.addEventListener('click', () => toggleDropdown());
+}
+
+function toggleDropdown()
+{
+    var ddContent = document.querySelector('.grid-drop')
+    if(ddContent.style.gridTemplateRows == "0fr")
+    {
+        ddContent.style.gridTemplateRows = "1fr"
+    }else{
+        ddContent.style.gridTemplateRows = "0fr"
+    }
+}
