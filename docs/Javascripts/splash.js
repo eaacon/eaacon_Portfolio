a94cb924970fae817dc9dc03f634f0de7431940d
Andrew Chen <andrewgychen@gmail.com>
2025-01-09 21:30:24 -0600

    DELETED

--- docs/Javascripts/splash.js
+++ docs/Javascripts/splash.js
@@ -3,18 +3,3 @@ const hiddenElements = document.querySelectorAll('.hidden')
 $("#splash").on('click', function(){
     $(location).attr('href', 'index.html#projects');
 });
-
-    // const observer = new IntersectionObserver((entries) => {
-    //     entries.forEach((entry) => {
-    //         console.log(entry)
-    //         if(entry.isIntersecting){
-    //             entry.target.classList.add('show');
-    //         }else{
-    //             entry.target.classList.remove('show');
-    //         }
-    //     });
-    // });
-
-    // hiddenElements.forEach((element) => {
-    //     observer.observe(element)
-    // });
