ca43435b7c78d39bc67cfc058bccbecbf3fb0f00
Andrew Chen <andrewgychen@gmail.com>
2025-01-10 22:25:05 -0600

    All Basic Functionality

--- docs/Javascripts/splash.js
+++ docs/Javascripts/splash.js
@@ -1,5 +1,23 @@
-const hiddenElements = document.querySelectorAll('.hidden')
+$(document).ready(function(){
+    $("#openvideo").on('click', function(){
+        console.log("Open Youtube Embed");
+        $('.navbar').fadeOut(100);
+        $('.modal').fadeIn(100);
+    });
 
-$("#splash").on('click', function(){
-    $(location).attr('href', 'index.html#projects');
+    $('#closevideo').on('click', function(){
+        console.log("Open Youtube Embed");
+        $('.navbar').fadeIn(100);
+        $('.modal').fadeOut(100);
+    });
+
+    $(document).on('keyup', function(event) {
+        if (event.key == "Escape" && $('.modal').is(':visible')) {
+            console.log("Close!");
+            console.log("Close Youtube Embed");
+            $('.navbar').fadeIn(100);
+            $('.modal').fadeOut(100);
+        }
+    });
 });
+
