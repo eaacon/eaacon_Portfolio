180c4b729ebaeb9a1947ccd6899c7d106a1555d5
Andrew Chen <andrewgychen@gmail.com>
2025-01-11 22:57:18 -0600

    New Contact + More Formatting

--- docs/Javascripts/index.js
+++ docs/Javascripts/index.js
@@ -2,13 +2,17 @@ $(document).ready(function(){
     $("#openvideo").on('click', function(){
         console.log("Open Youtube Embed");
         $('.navbar').fadeOut(100);
-        $('.modal').fadeIn(100);
+        $('.modal').fadeIn(100, function(){
+            $('.backdropvideo').addClass('blur')
+        });
     });
 
     $('#closevideo').on('click', function(){
         console.log("Open Youtube Embed");
         $('.navbar').fadeIn(100);
-        $('.modal').fadeOut(100);
+        $('.modal').fadeOut(100, function(){
+            $('.backdropvideo').removeClass('blur')
+        });
     });
 
     $(document).on('keyup', function(event) {
@@ -16,8 +20,17 @@ $(document).ready(function(){
             console.log("Close!");
             console.log("Close Youtube Embed");
             $('.navbar').fadeIn(100);
-            $('.modal').fadeOut(100);
+            $('.modal').fadeOut(100, function(){
+                $('.backdropvideo').removeClass('blur')
+            });
         }
     });
+
+    $(document).on('mousemove', function(e){
+        $('.tooltip').css({
+            left: e.pageX + 16,
+            top: e.pageY + 16
+        })
+    })
 });
 
